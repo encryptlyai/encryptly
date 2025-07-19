5a73db4101c753e74fbf07c03b30f35d1d68be00
aydinefendi <aydinefendi27@gmail.com>
2025-07-19 13:33:37 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="encryptly",
-    version="0.1.1",
+    version="0.1.2",
     author="Encryptly Team",
     author_email="support@encryptly.com",
     description="Lightweight Authentication for AI Agents",
